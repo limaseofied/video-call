1a035bf4378cbf3a5b7e7360b73d1f9f6ea80656
Banashri <banashri.mohanty@csm.tech>
2026-01-13 14:45:42 +0530

    changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'video-call-app'`, () => {
+  it(`should have as title 'video-call'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('video-call-app');
+    expect(app.title).toEqual('video-call');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('video-call-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('video-call app is running!');
   });
 });
